2094bd5e07fbdb86e4fbdbebceb26c7137ccedca
A. Ar <aeaerkam@gmail.com>
2021-03-21 13:43:15 +0300

    ShapesCanvas2.java

---
+++ ShapesCanvas2.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import javax.swing.*;  
+
+public class ShapesCanvas2 extends JPanel
+{
+	// Variables
+	private ShapeContainer shapes;
+	
+	// Constructor
+    public ShapesCanvas(ShapeContainer shapes) 
+    {
+   		this.shapes = shapes;
+    	
+    	setPreferredSize (new Dimension(500, 500));		
+    }
+    
+    // draws the panel by requesting the ShapeContainer to draw itself
+    public void paintComponent(Graphics g)
+    {
+    	super.paintComponent (g);
+    	
+    	shapes.draw (g);	
+    }	
+    
+    
+}
